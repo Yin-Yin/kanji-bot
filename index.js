bb954ee20273ff197d683d93a4495b6fc521292c
Doktor Schwein <doktorschwein@gmail.com>
2019-01-10 18:06:13 +0000

    first version of server added

---
+++ index.js
@@ -0,0 +1,30 @@
+'use strict';
+// ## express server ##
+var bodyParser = require('body-parser')
+var express = require('express');
+var app = express();
+var dialogflowModule = require('./dialogflow/dialogflow.js');
+
+console.log("starting server ..");
+//firestoreModule.initializeDatabase();
+
+// parse application/x-www-form-urlencoded 
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+
+app.post('/', function(req, res) {
+  console.log("app post /");
+  dialogflowModule.handleRequest(req).then((responseJSON) => {
+    res.send(responseJSON); 
+    console.log("responding with" + responseJSON);
+  });
+})
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
