4b5ca766ad02caf779de090a77a9d53fe9d1d937
Doktor Schwein <doktorschwein@gmail.com>
2019-01-11 21:47:59 +0000

    test API call

--- dialogflow/dialogflow.js
+++ dialogflow/dialogflow.js
@@ -39,7 +39,7 @@ module.exports = {
             optionsKanjialiveRapidapi.path = '/api/public/kanji/' + encodeURIComponent(request.body.queryResult.parameters.kanji_single);
             console.log("optionsKanjialiveRapidapi", optionsKanjialiveRapidapi);
             makeHttpsRequest(optionsKanjialiveRapidapi);
-            agent.add(`Your Kanji is ` + request.body.queryResult.parameters.kanji);
+            agent.add(`Your Kanji is ` + request.body.queryResult.parameters.kanji_single);
         }
 
         // Uncomment and edit to make your own intent handler
@@ -87,7 +87,7 @@ module.exports = {
                 console.log(`statusCode: ${res.statusCode}`)
 
                 res.on('data', (d) => {
-                    process.stdout.write(d);
+                    //process.stdout.write(d);
                     console.log('data: ', d);
                 })
             })
