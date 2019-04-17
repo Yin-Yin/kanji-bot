2c7dc2b5c517933506dde81f518eefb7d9b7c32c
Doktor Schwein <doktorschwein@gmail.com>
2019-04-17 15:03:56 +0000

    different output for kanji explain to see if it remediates the bug

--- dialogflow/dialogflow.js
+++ dialogflow/dialogflow.js
@@ -39,21 +39,18 @@ module.exports = {
         function kanjiExplain(agent) {
             let kanjiData = kanjiModule.getKanjiData(parameters.kanji_single);
             console.log('kanjiData', kanjiData);
-            /*
             agent.add(new Card({
                 title: 'Kanji: ' + kanjiData.kanji.character,
                 imageUrl: kanjiData.kanji.video.poster,
-                text: 'Meaning: ' + kanjiData.kanji.meaning.english + '\n Radical: ' + kanjiData.radical.character + '\n Onyomi: ' + kanjiData.kanji.onyomi.katakana + ' (' + kanjiData.kanji.onyomi.romaji + ') \n Kunyomi: ' + kanjiData.kanji.kunyomi.hiragana + ' (' + kanjiData.kanji.kunyomi.romaji + ')',
-                //buttonText: 'Listen to example',
-                //buttonUrl: kanjiData.examples[index].audio.mp3
+                text: 'Meaning: ' + kanjiData.kanji.meaning.english + '\n Radical: ' + kanjiData.radical.character + '\n Onyomi: ' + kanjiData.kanji.onyomi.katakana + ' (' + kanjiData.kanji.onyomi.romaji + ') \n Kunyomi: ' + kanjiData.kanji.kunyomi.hiragana + ' (' + kanjiData.kanji.kunyomi.romaji + ')'
             }));
-            */
+            /*
             agent.add('Kanji: ' + kanjiData.kanji.character);
             agent.add('Meaning: ' + kanjiData.kanji.meaning.english);
             //agent.add('Radical: ' + kanjiData.radical.character);
             //agent.add('Onyomi: ' + kanjiData.kanji.onyomi.katakana + ' (' + kanjiData.kanji.onyomi.romaji + ')');
             //agent.add('Kunyomi: ' + kanjiData.kanji.kunyomi.hiragana + ' (' + kanjiData.kanji.kunyomi.romaji + ')');
-            
+            */
             agent.add('Example:' + kanjiData.examples[0].japanese);
             //agent.add(new Suggestion(`Radical ` + kanjiData.radical.character));
             agent.add(new Suggestion(`Examples ` + kanjiData.kanji.character));
